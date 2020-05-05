89b98160671a9c8e64e2b1ca299019840e7ddcf7
guillaumefaugeron <guillaume.faugeron@ynov.com>
2020-05-05 11:42:09 +0200

    Creation of slide 12 workshop without responsibility

---
+++ blocks/block-imagineofive.php
@@ -0,0 +1,30 @@
+
+<div class="testimonial-block">
+    <div id="cercle">
+
+    </div>
+    <div id="cercle_second">
+        <p>Environ 5 min</p>
+        <p>En cercle, assis</p>
+
+    </div>
+
+    <div id="cercle_third">
+        <p class="paragraph"><?php block_field( 'contentcercle2' ); ?></p>
+
+    </div>
+
+    <div id="cercle_four">
+        <p class="paragraph"><?php block_field( 'contentcercle1' ); ?></p>
+
+    </div>
+
+    <div>
+        <img id="logo_imagineo" src="<?php echo get_template_directory_uri()."../img/logo_imagineo.png"?>" alt="logo_imagineo">
+    </div>
+    <div class="testimonial-block__content">
+        <h2 class="title"><?php block_field( 'titre' ); ?></h2>
+        <p class="soustitre"><?php block_field( 'soustitre' ); ?></p>
+        <p class="paragraph"><?php block_field( 'text' ); ?></p>
+    </div>
+</div> 
