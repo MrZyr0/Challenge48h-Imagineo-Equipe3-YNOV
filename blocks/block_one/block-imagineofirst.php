5d178b67e0cd3320566a715db1f6258a946e0bb3
Enzo.Sborea <enzo.sborea@ynov.com>
2020-05-05 14:53:29 +0200

    last commit

---
+++ blocks/block_one/block-imagineofirst.php
@@ -0,0 +1,12 @@
+
+<div class="testimonial-block">
+    <div id="cercle"></div>
+    <div id="cercle_second"></div>
+    <div id="cercle_third"></div>
+    <div id="cercle_four"></div>
+    <div><img id="logo_imagineo" src="<?php echo get_theme_root_uri()."/imagineo/blocks/img/logo_imagineo.png"?>" alt="logo_imagineo"></div>
+    <div class="testimonial-block__content">
+        <h2 class="title"><?php block_field( 'titre' ); ?></h2>
+        <p class="paragraph"><?php block_field( 'soustitre' ); ?></p>
+    </div>
+</div> 
---
+++ blocks/block_two/block-imagineosecond.php
@@ -0,0 +1,13 @@
+
+<div class="testimonial-block">
+    <div class="testimonial-block__content">
+        <div id="circle"></div>
+        <div id="circle_two"></div>
+
+        <p id="areatitle"><?php block_field( 'areatitre_block_two' ); ?></p>
+        <p id="area"><?php block_field( 'area_block_two' ); ?></p>
+        <p id="areatitle_two"><?php block_field( 'areatitre_two_block_two' ); ?></p>
+        <p id="areatwo"><?php block_field( 'area_two_block_two' ); ?></p>
+        <p id="areathree"><?php block_field( 'soustitre_block_two' ); ?></p>
+    </div>
+</div> 
